3d56687d473cf2e6e6130e43fe7437c8080ba865
mpatfield <martin@patfield.com>
2025-08-29 09:45:59 -0700

    Genericize validation

--- src/tools/validation.ts
+++ src/tools/validation.ts
@@ -4,11 +4,10 @@ import { strings } from '../i18n/i18n.js';
 
 import { Assertable } from '../model/types.js';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function assert(log: Log, caller: string, assertable: Assertable, ...keys: (keyof any)[]): boolean {
+export function assert<A extends Assertable>(log: Log, caller: string, assertable: A, ...keys: (keyof A)[]): boolean {
   let valid = true;
   for (const key of keys) {
-    if ((assertable as Record<string, unknown>)[key as string] === undefined) {
+    if (assertable[key] === undefined) {
       log.error(strings.accessory.missingRequired, caller, `'${String(key)}'`);
       valid = false;
     }
