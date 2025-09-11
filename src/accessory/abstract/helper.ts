1773b5863bb1e9d74474f069ecb3b62ea887ad4b
mpatfield <martin@patfield.com>
2025-09-11 16:52:13 -0700

    Move accessory creation to helpers file

---
+++ src/accessory/abstract/helper.ts
@@ -0,0 +1,68 @@
+import { PlatformAccessory } from 'homebridge';
+
+import { BaseAccessory } from './base.js';
+
+import { AccessoryType } from '../../model/enums.js';
+import * as Configs from '../../model/types.js';
+
+import { LockMechanismAccessory } from '../lock.js';
+import { SecuritySystemAccessory } from '../security.js';
+import { ThermostatAccessory } from '../thermostat.js';
+
+import { LightbulbAccessory } from '../onoff/lightbulb.js';
+import { OutletAccessory } from '../onoff/outlet.js';
+import { SwitchAccessory } from '../onoff/switch.js';
+
+import { CO2SensorAccessory } from '../sensor/carbonDioxide.js';
+import { COSensorAccessory } from '../sensor/carbonMonoxide.js';
+import { ContactSensorAccessory } from '../sensor/contact.js';
+import { HumiditySensorAccessory } from '../sensor/humidity.js';
+import { LeakSensorAccessory } from '../sensor/leak.js';
+import { MotionSensorAccessory } from '../sensor/motion.js';
+import { OccupancySensorAccessory } from '../sensor/occupancy.js';
+import { SmokeSensorAccessory } from '../sensor/smoke.js';
+import { TemperatureSensorAccessory } from '../sensor/temperature.js';
+
+import { Log } from '../../tools/log.js';
+
+export function createAccessory(
+  Service: Configs.ServiceType,
+  Characteristic: Configs.CharacteristicType,
+  platformAccessory: PlatformAccessory,
+  accessoryConfig: Configs.BaseAccessoryConfig,
+  log: Log,
+): BaseAccessory | undefined {
+
+  switch(accessoryConfig.info.type) {
+  case AccessoryType.CarbonDioxideSensor:
+    return new CO2SensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.CO2SensorConfig, log);
+  case AccessoryType.CarbonMonoxideSensor:
+    return new COSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.COSensorConfig, log);
+  case AccessoryType.ContactSensor:
+    return new ContactSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.ContactSensorConfig, log);
+  case AccessoryType.HumiditySensor:
+    return new HumiditySensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.HumiditySensorConfig, log);
+  case AccessoryType.LeakSensor:
+    return new LeakSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.LeakSensorConfig, log);
+  case AccessoryType.Lightbulb:
+    return new LightbulbAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.LightbulbConfig, log);
+  case AccessoryType.LockMechanism:
+    return new LockMechanismAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.LockMechanismConfig, log);
+  case AccessoryType.MotionSensor:
+    return new MotionSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.MotionSensorConfig, log);
+  case AccessoryType.OccupancySensor:
+    return new OccupancySensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.OccupancySensorConfig, log);
+  case AccessoryType.Outlet:
+    return new OutletAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.OutletConfig, log);
+  case AccessoryType.SecuritySystem:
+    return new SecuritySystemAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.SecuritySystemConfig, log);
+  case AccessoryType.SmokeSensor:
+    return new SmokeSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.SmokeSensorConfig, log);
+  case AccessoryType.Switch:
+    return new SwitchAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.SwitchConfig, log);
+  case AccessoryType.TemperatureSensor:
+    return new TemperatureSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.TemperatureSensorConfig, log);
+  case AccessoryType.Thermostat:
+    return new ThermostatAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.ThermostatConfig, log);
+  }
+}
--- src/homebridge/platform.ts
+++ src/homebridge/platform.ts
@@ -3,27 +3,11 @@ import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig }
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 
 import { BaseAccessory } from '../accessory/abstract/base.js';
-
-import { ContactSensorAccessory } from '../accessory/sensor/contact.js';
-import { CO2SensorAccessory } from '../accessory/sensor/carbonDioxide.js';
-import { COSensorAccessory } from '../accessory/sensor/carbonMonoxide.js';
-import { HumiditySensorAccessory } from '../accessory/sensor/humidity.js';
-import { LeakSensorAccessory } from '../accessory/sensor/leak.js';
-import { LightbulbAccessory } from '../accessory/onoff/lightbulb.js';
-import { LockMechanismAccessory } from '../accessory/lock.js';
-import { MotionSensorAccessory } from '../accessory/sensor/motion.js';
-import { OccupancySensorAccessory } from '../accessory/sensor/occupancy.js';
-import { OutletAccessory } from '../accessory/onoff/outlet.js';
-import { SecuritySystemAccessory } from '../accessory/security.js';
-import { SmokeSensorAccessory } from '../accessory/sensor/smoke.js';
-import { SwitchAccessory } from '../accessory/onoff/switch.js';
-import { TemperatureSensorAccessory } from '../accessory/sensor/temperature.js';
-import { ThermostatAccessory } from '../accessory/thermostat.js';
+import { createAccessory } from '../accessory/abstract/helper.js';
 
 import { setLanguage, strings } from '../i18n/i18n.js';
 
-import { AccessoryType } from '../model/enums.js';
-import * as Configs from '../model/types.js';
+import { BaseAccessoryConfig } from '../model/types.js';
 
 import { Log } from '../tools/log.js';
 import getVersion from '../tools/version.js';
@@ -34,7 +18,7 @@ export class HomebridgeEasyMQTT implements DynamicPlatformPlugin {
   private readonly log: Log;
 
   private readonly platformAccessories: Map<string, PlatformAccessory> = new Map();
-  private readonly accessories: BaseAccessory<Configs.BaseAccessoryConfig>[] = [];
+  private readonly accessories: BaseAccessory<BaseAccessoryConfig>[] = [];
 
   constructor(
     logger: Logger,
@@ -84,7 +68,7 @@ export class HomebridgeEasyMQTT implements DynamicPlatformPlugin {
 
     const keepIdentifiers = new Set<string>();
 
-    for (const accessoryConfig of this.config.accessories as Configs.BaseAccessoryConfig[]) {
+    for (const accessoryConfig of this.config.accessories as BaseAccessoryConfig[]) {
 
       if (!assert(this.log, PLATFORM_NAME, accessoryConfig, 'info') ||
         !assert(this.log, PLATFORM_NAME, accessoryConfig.info, 'name', 'type')) {
@@ -110,54 +94,9 @@ export class HomebridgeEasyMQTT implements DynamicPlatformPlugin {
       const Service = this.api.hap.Service;
       const Characteristic = this.api.hap.Characteristic;
 
-      let accessory: BaseAccessory<Configs.BaseAccessoryConfig>;
-      switch(accessoryConfig.info.type) {
-      case AccessoryType.CarbonDioxideSensor:
-        accessory = new CO2SensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.CO2SensorConfig, this.log);
-        break;
-      case AccessoryType.CarbonMonoxideSensor:
-        accessory = new COSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.COSensorConfig, this.log);
-        break;
-      case AccessoryType.ContactSensor:
-        accessory = new ContactSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.ContactSensorConfig, this.log);
-        break;
-      case AccessoryType.HumiditySensor:
-        accessory = new HumiditySensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.HumiditySensorConfig, this.log);
-        break;
-      case AccessoryType.LeakSensor:
-        accessory = new LeakSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.LeakSensorConfig, this.log);
-        break;
-      case AccessoryType.Lightbulb:
-        accessory = new LightbulbAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.LightbulbConfig, this.log);
-        break;
-      case AccessoryType.LockMechanism:
-        accessory = new LockMechanismAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.LockMechanismConfig, this.log);
-        break;
-      case AccessoryType.MotionSensor:
-        accessory = new MotionSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.MotionSensorConfig, this.log);
-        break;
-      case AccessoryType.OccupancySensor:
-        accessory = new OccupancySensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.OccupancySensorConfig, this.log);
-        break;
-      case AccessoryType.Outlet:
-        accessory = new OutletAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.OutletConfig, this.log);
-        break;
-      case AccessoryType.SecuritySystem:
-        accessory = new SecuritySystemAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.SecuritySystemConfig, this.log);
-        break;
-      case AccessoryType.SmokeSensor:
-        accessory = new SmokeSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.SmokeSensorConfig, this.log);
-        break;
-      case AccessoryType.Switch:
-        accessory = new SwitchAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.SwitchConfig, this.log);
-        break;
-      case AccessoryType.TemperatureSensor:
-        accessory = new TemperatureSensorAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.TemperatureSensorConfig, this.log);
-        break;
-      case AccessoryType.Thermostat:
-        accessory = new ThermostatAccessory(Service, Characteristic, platformAccessory, accessoryConfig as Configs.ThermostatConfig, this.log);
-        break;
-      default:
+      const accessory = createAccessory(Service, Characteristic, platformAccessory, accessoryConfig, this.log);
+
+      if (accessory === undefined) {
         this.log.error(strings.startup.unsupportedType, accessoryConfig.info.type);
         continue;
       }
