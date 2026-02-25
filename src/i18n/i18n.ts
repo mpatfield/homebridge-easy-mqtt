90b936c983f1dd7b7c48f9055d4ccb48ba6e91ca
mpatfield <martin@patfield.com>
2026-02-25 15:50:09 -0800

    Add missing translations

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,21 +1,27 @@
+import da from './da.js';
 import de from './de.js';
 import en from './en.js';
 import fr from './fr.js';
+import it from './it.js';
 import ro from './ro.js';
 import vi from './vi.js';
 
 export enum Language {
+  DA = 'da',
   DE = 'de',
   EN = 'en',
   FR = 'fr',
+  IT = 'it',
   RO = 'ro',
   VI = 'vi',
 }
 
 const Translations: Record<Language, Translation> = {
+  [Language.DA]: da,
   [Language.DE]: de,
   [Language.EN]: en,
   [Language.FR]: fr,
+  [Language.IT]: it,
   [Language.RO]: ro,
   [Language.VI]: vi,
 };
